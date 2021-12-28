3c0035872f07d92fed6cca673b85619f1144b7bd
Pablo Montenegro <montenegro.pablo@outlook.com>
2021-12-28 23:10:38 +0100

    fix creating video

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const videoshow = require('videoshow');
 
 const { readFileSync, writeFileSync, mkdirSync, rmdirSync } = require('fs');
 
-const { WIDTH, HEIGHT } = require("./sizes");
+const { WIDTH, HEIGHT, MAX_LINES, SCALE } = require("./sizes");
 
-const createScreenshot = async (html, filePath) => {
+const createScreenshot = async (html, filePath, posY) => {
     const browser = await puppeteer.launch({
         headless: true,
         args: [`--window-size=${WIDTH},${HEIGHT}`],
@@ -20,15 +20,18 @@ const createScreenshot = async (html, filePath) => {
 
     const page = await browser.newPage();
     page.setContent(html);
+    await page.evaluate((scrollY) => {
+        window.scrollBy(0, scrollY);
+    }, posY);
 
     await page.screenshot({
         path: filePath,
         // omitBackground: true,
         // clip: {
-        //     x: rect.left - padding,
-        //     y: rect.top - padding,
-        //     width: rect.width + padding * 2,
-        //     height: rect.height + padding * 2
+        //     y: posY,
+        //     x: 0,
+        //     width: WIDTH,
+        //     height: HEIGHT
         // }
     });
 
@@ -38,7 +41,7 @@ const createScreenshot = async (html, filePath) => {
 const createVideo = (images) => {
     const videoOptions = {
         fps: 25,
-        loop: 2,
+        loop: 2, // seconds
         transition: false,
         videoBitrate: 1024,
         videoCodec: 'libx264',
@@ -47,19 +50,20 @@ const createVideo = (images) => {
         audioChannels: 2,
         format: 'mp4',
         pixelFormat: 'yuv420p'
-    }
+    };
 
     return videoshow(images, videoOptions)
         .save('./video.mp4')
         .on('start', function (command) {
-            console.log('ffmpeg process started:', command)
+            console.log('ffmpeg process started:', command);
         })
         .on('error', function (err, stdout, stderr) {
-            console.error('Error:', err)
-            console.error('ffmpeg stderr:', stderr)
+            console.error('Error:', err);
+            console.error('ffmpeg stdout:', stdout);
+            console.error('ffmpeg stderr:', stderr);
         })
         .on('end', function (output) {
-            console.error('Video created in:', output)
+            console.error('Video created in:', output);
         })
 }
 
@@ -74,28 +78,39 @@ const generateFiles = async (filePath) => {
     // const code = prettier.format(data, { parser: "babel" });
     const lines = code.split('\n');
 
-    const html = generateHtml(code, lines.length, lines.length);
+    // const html = generateHtml(code, lines.length, lines.length);
     // writeFileSync("./html/index.html", html);
 
     const images = [];
     let index = 0;
     let codeToParse = '';
+    let posX = 21;
+    const lineToStartScrolling = (MAX_LINES / 2) + 1;
     for (const line of lines) {
+        index += 1;
         const filePath = `${fileOutput}${index}.png`;
 
         codeToParse = `${codeToParse}${line}\n`;
-        const html = generateHtml(codeToParse, index + 1, lines.length);
-        writeFileSync(`./html/index-${index}.html`, html);
+        const html = generateHtml(codeToParse, index, lines.length);
+        // writeFileSync(`./html/index-${index}.html`, html);
         console.log(`Creating image: ${filePath}`);
-        await createScreenshot(html, filePath);
+        await createScreenshot(
+            html,
+            filePath,
+            index > lineToStartScrolling ? (posX * SCALE) : 0
+        );
+
+        if (index > lineToStartScrolling) {
+            posX += 16;
+        }
+
         console.log('Done!');
         images.push(filePath);
-        index += 1;
     }
 
     console.log('Creating video...')
     await createVideo(images);
 }
 
-const file = process.argv[2] || './Test.jsx';
-generateFiles(file);
+const filePath = process.argv[2] || 'Test.jsx';
+generateFiles(filePath);
--- sizes.js
+++ sizes.js
@@ -2,4 +2,4 @@ export const WIDTH = Math.floor(1920);
 // export const HEIGHT = Math.floor(1058 / 4);
 export const HEIGHT = Math.floor(1080);
 export const SCALE = 4;
-export const MAX_LINES = 17;
+export const MAX_LINES = 16;
